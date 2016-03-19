18f63af5c023e2af9e7c9a8a10c54e11a93e8472
LY <ly.franky@gmail.com>
2016-03-19 20:08:51 +0800

    first initial

---
+++ Caculator/Assets/Caculate.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Caculate : MonoBehaviour {
+
+	private string[] buttonText = {
+		"c",
+		"del",
+		"/",
+		"*",
+		"7",
+		"8",
+		"9",
+		"-",
+		"4",
+		"5",
+		"6",
+		"+",
+		"1",
+		"2",
+		"3",
+		"=",
+		"(",
+		"0",
+		".",
+		")"
+	};
+	string displayNum;
+	// Use this for initialization
+	void Start () {
+		displayNum = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		createDisplayScreen ();
+		for (int i = 0; i < 20; i++) {
+			if (GUI.Button(new Rect((i % 4) * 30, (i / 4) * 20 + 20, 30, 20), buttonText[i])) {
+				if (this.buttonText[i].Equals("c")) {
+					clear();
+				} else if (this.buttonText[i].Equals("del")) {
+					del();
+				} else if (this.buttonText[i].Equals("=")) {
+					caculate();
+				} else {
+					displayNum += this.buttonText[i];
+					print (displayNum);
+				}
+			}
+		}
+
+	}
+
+	void del() {
+		displayNum = displayNum.Substring (0, displayNum.Length - 1);
+	}
+
+	void clear() {
+		displayNum = "";
+	}
+
+	void showNum() {
+		
+	}
+
+	void createDisplayScreen() {
+		GUI.Button (new Rect (0, 0, 120, 20), displayNum);
+	}
+
+	void caculate()  {
+		clear ();
+		print ("fdjss");
+	}
+}
---
+++ FirstProject/Assets/CreateGameObject.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateGameObject : MonoBehaviour {
+
+	void Awake() {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		//create GameObject
+		/*
+		GameObject testCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		testCube.transform.position = new Vector3 (0f, 2f, 0f);
+
+		GameObject prefab = Instantiate (Resources.Load("cubePrefab")) as GameObject;
+		prefab.transform.transform.position = new Vector3 (0f, 1f, 0f);
+
+		testCube.transform.parent = this.transform;
+		*/
+		GameObject cube = GameObject.Find ("Cube");
+		cube.transform.position = new Vector3(1f, 0f, 0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (0, Random.Range (0, 5), 0);
+	}
+
+	void FixedUpdate() {
+		
+	}
+
+	void LateUpdate() {
+		
+	}
+
+	void OnGUI() {
+		
+	}
+
+	void Reset() {
+	
+	}
+
+	void OnDisable() {
+	
+	}
+
+	void OnDestroy() {
+	
+	}
+}
